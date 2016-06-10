dac7f4f1998c210349fbf1438b64af96b58fee4c
João Vicente Souto <joaovicentesouto@gmail.com>
2016-06-09 21:31:28 -0300

    First commit

---
+++ src/Aviao.java
@@ -0,0 +1,71 @@
+
+public class Aviao {
+
+	private double velocidade;
+	private int combustivel;
+	private String direcaoVoo;
+	private int[] proximaPosicao = new int[2];
+	
+	public Aviao() { // Construtor sem parametros.
+		super();
+	}
+	
+	// Construtor com parametros.
+	public Aviao(double velocidade, int combustivel, String direcaoVoo, int proximoX, int proximoY) {
+		this.velocidade = velocidade;
+		this.combustivel = combustivel;
+		this.direcaoVoo = direcaoVoo;
+		proximaPosicao[0] = proximoX;
+		proximaPosicao[1] = proximoY;
+	}
+	
+	// Getters e setters.
+	public double getVelocidade() {
+		return velocidade;
+	}
+
+	public void setVelocidade(double velocidade) {
+		this.velocidade = velocidade;
+	}
+
+	public int getCombustivel() {
+		return combustivel;
+	}
+
+	public void setCombustivel(int combustivel) {
+		this.combustivel = combustivel;
+	}
+
+	public String getDirecaoVoo() {
+		return direcaoVoo;
+	}
+
+	public void setDirecaoVoo(String direcaoVoo) {
+		this.direcaoVoo = direcaoVoo;
+	}
+
+	public int[] getProximaPosicao() {
+		return proximaPosicao;
+	}
+
+	public void setProximaPosicao(int[] proximaPosicao) {
+		this.proximaPosicao = proximaPosicao;
+	}
+	
+	// Getters da proxima posição X e Y
+	public int getProximaPosicaoX() {
+		return proximaPosicao[0];
+	}
+	
+	public int getProximaPosicaoY() {
+		return proximaPosicao[1];
+	}
+	
+	// Método para generalizar a locomoção do avião. Pega a direcão do avião e define a proxima posicao --- FAZER ISSO NO ESPAÇO AEREO.
+	
+	
+	
+	
+	
+	
+}
---
+++ src/AviaoTurbo.java
@@ -0,0 +1,17 @@
+
+public class AviaoTurbo extends Aviao {
+
+	boolean turbo = true;
+	
+	public AviaoTurbo() {
+		super();
+	}
+	
+	public AviaoTurbo(double velocidade, int combustivel, String direcaoVoo, int proximoX, int proximoY){
+		super(velocidade, combustivel, direcaoVoo, proximoX, proximoY);
+	}
+	
+	public void velocTurbo() {
+		super.setVelocidade(super.getVelocidade()*1.25);
+	}
+}
